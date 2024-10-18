f0d70527954de3bc217eee1886116a50b4fe004a
Luis Villarreal <luisxdkit@gmail.com>
2024-10-18 12:12:16 -0500

    splash screen y inicio rapido

---
+++ app/src/main/java/com/example/ima_tienda/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.ima_tienda
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash) // Asegúrate de que este archivo XML esté configurado correctamente.
+
+        // Duración del Splash Screen
+        val splashScreenDuration = 2000L
+
+        // Inicia la MainActivity después de la duración del splash
+        android.os.Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish() // Finaliza SplashActivity para que no regrese con el botón atrás
+        }, splashScreenDuration)
+    }
+}
+
